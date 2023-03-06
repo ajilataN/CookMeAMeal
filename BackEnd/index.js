d53fe4a1e8e9f5712ca960875c337d661e52d4ea
ajialataN <89211061@student.upr.si>
2023-03-06 09:49:36 +0100

    Added database, unsuccessful connection.

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 
 const app = express()
+const dotenv = require("dotenv")
+dotenv.config()
 
 const port = process.env.PORT || 5018
 
 //Import opur custom modules-controllers
 const meal= require("./routes/meal")
+const db = require("./db/conn")
 //Routes
 app.use('/meal', meal);
 
